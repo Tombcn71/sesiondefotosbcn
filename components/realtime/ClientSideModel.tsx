d63652092a17c5dd0311c6c78e66495f839414e6
Tom <tbvanreijn@gmail.com>
2024-06-27 17:39:15 +0200

    fini

--- components/realtime/ClientSideModel.tsx
+++ components/realtime/ClientSideModel.tsx
@@ -50,7 +50,7 @@ export default function ClientSideModel({
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-0">
           {samples && (
             <div className="flex w-full lg:w-1/2 flex-col gap-2">
-              <h2 className="text-xl">Training Data</h2>
+              <h2 className="text-xl">Muestras</h2>
               <div className="flex flex-row gap-4 flex-wrap">
                 {samples.map((sample) => (
                   <img
@@ -62,9 +62,9 @@ export default function ClientSideModel({
             </div>
           )}
           <div className="flex flex-col w-full lg:w-1/2 rounded-md">
-            {model.status === "finished" && (
+            {model.status === "finalizado" && (
               <div className="flex flex-1 flex-col gap-2">
-                <h1 className="text-xl">Results</h1>
+                <h1 className="text-xl">Resultados</h1>
                 <div className="flex flex-row flex-wrap gap-4">
                   {serverImages?.map((image) => (
                     <div key={image.id}>
